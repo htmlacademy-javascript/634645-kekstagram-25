91cef65eeb5f6996e7e1627c071ee6b7c8e6948a
brylev.v <iam-vova@yandex.ru>
2022-03-30 22:18:06 +0300

    M2T1 | Добавляет функции checkCommentLength и getRandomNumber

---
+++ js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const COMMENT_MAX_LENGTH = 140;
+
+const getRandomNumber = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const checkCommentLength = (string, maxLength) => string.length <= maxLength;
+
+getRandomNumber(0, 10);
+checkCommentLength('comment', COMMENT_MAX_LENGTH);
